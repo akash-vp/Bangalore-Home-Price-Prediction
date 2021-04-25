a1c3c332cde38fb486fa5e277f6a32148e61e07a
akash_vp <43694492+akash-vp@users.noreply.github.com>
2021-04-25 12:54:53 +0530

    Add files via upload

---
+++ util.py
@@ -0,0 +1,53 @@
+import json
+import pickle
+import numpy as np
+    
+    
+    
+__locations = None
+__data_columns = None
+__model = None
+
+
+def get_location_names():
+    return __locations
+
+def get_data_columns():
+    return __data_columns
+
+def get_estimated_price(location,sqft,bhk,bath):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+    
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+        
+    return round(__model.predict([x])[0],2)
+
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global __data_columns
+    global __locations
+    
+    with open("columns.json",'r') as f:
+        __data_columns= json.load(f)['data_columns']
+        __locations = __data_columns[3:] # first 3 columns are sqft, bath, bhk
+        
+    global __model
+    if __model is None:
+        with open("bangalore_home_price_model.pickle",'rb') as f:
+            __model = pickle.load(f)
+    print("Loading artifacts is done....")
+    
+    
+if __name__ == '__main__':
+    load_saved_artifacts()
+    print(get_location_names())
+    print(get_estimated_price('1st Phase jp nagar', 1000, 3, 3))
